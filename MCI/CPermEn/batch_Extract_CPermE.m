54284cbf9083c2429154efe67e2e8fedd9699c5f
monk5469 <34763521+monk5469@users.noreply.github.com>
2025-01-13 09:54:13 +0800

    Add files via upload

---
+++ MCI/CPermEn/batch_Extract_CPermE.m
@@ -0,0 +1,45 @@
+clear all;
+close all;
+
+Num       =  [32 32];
+Group     =   {'MCI' 'NC'};
+k=1;
+
+f = waitbar(0,'Extracting features, please wait...');
+
+for g  = 1 : length(Group)
+
+    for nSub = 1 : Num(g)
+        ntmp = ( (g-1)*Num(1)+nSub ) / sum(Num) ;
+        str=['Extracting features, please wait...',num2str( round (100* ntmp) ),'%'];
+        waitbar( ntmp, f, str);
+
+        if strcmp(Group(g),'MCI')
+            load([ 'CPermE_data\MCI_CPermE_', num2str(nSub), '.mat']);
+            tmp = Mat (RHO);
+            tmp=num2cell(tmp);
+            for num = 1: length(tmp)
+                X { k,  num } = tmp {num};
+            end
+            Y  {k, 1}  = 'MCI';
+            k=k+1;
+        elseif strcmp(Group(g),'NC')
+            load([ 'CPermE_data\NC_CPermE_', num2str(nSub), '.mat']);
+            tmp = Mat (RHO);
+            tmp=num2cell(tmp);
+            for num = 1: length(tmp)
+                X { k,  num } = tmp {num};
+            end
+            Y  {k, 1}  = 'NC';
+            k=k+1;
+        end
+
+    end
+
+end
+
+save('MCI_NC_CPermE.mat','X','Y');
+close(f);
+
+
+
