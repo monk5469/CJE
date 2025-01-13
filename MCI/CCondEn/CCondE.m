1ccdfb989ba7fb44ed955a36d40c95b66959714f
monk5469 <34763521+monk5469@users.noreply.github.com>
2025-01-13 09:48:17 +0800

    Add files via upload

---
+++ MCI/CCondEn/CCondE.m
@@ -0,0 +1,34 @@
+function Y = CCondE (X)
+%%
+% YM            - a cell-type value, M*M timeseries cross-spectrum
+%                 information
+% X             - an M*M timeseries matrix
+
+%%
+% x = reshape(X,1,size(X,1)*size(X,2));
+% x = normalize(x);
+% X=reshape(x,size(X,1),size(X,2));
+
+%%max_min
+% x = reshape(X,1,size(X,1)*size(X,2));
+% x1 = (x-min(x))/(max(x)-min(x));
+% X = reshape(x1,size(X,1),size(X,2));
+for m=1:size(X,2)
+    for n=1:size(X,2)
+        if m == n
+            Y{m,n} = 0;
+        else
+%             X(:,m) = normalize(X(:,m));
+%             X(:,n) = normalize(X(:,n));
+%             X(:,m) = (X(:,m)-min(X(:,m)))/(max(X(:,m))-min(X(:,m)));
+%             X(:,n) = (X(:,n)-min(X(:,n)))/(max(X(:,n))-min(X(:,n)));
+            Sig=[X(:,m),X(:,n)];
+            z = XCondEn (Sig);
+            Y{m,n}=z(1,1);
+        end
+    end
+end
+
+end
+
+
