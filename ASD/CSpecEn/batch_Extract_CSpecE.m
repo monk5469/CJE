85db81dc985babafc84e5773fba408cc7645c5b2
monk5469 <34763521+monk5469@users.noreply.github.com>
2025-01-13 09:20:37 +0800

    Add files via upload

---
+++ ASD/CSpecEn/batch_Extract_CSpecE.m
@@ -0,0 +1,40 @@
+clear all;
+close all;
+
+Num       =  [48 50];
+Group     =   {'ASD' 'NC'};
+k=1;
+
+f = waitbar(0,'Extracting features, please wait...');
+
+for g  = 1 : length(Group)
+
+    for nSub = 1 : Num(g)
+        ntmp = ( (g-1)*Num(1)+nSub ) / sum(Num) ;
+        str=['Extracting features, please wait...',num2str( round (100* ntmp) ),'%'];
+        waitbar( ntmp, f, str);
+
+        if strcmp(Group(g),'ASD')
+            load([ 'CSpecE_data\ASD_CSpecE_', num2str(nSub), '.mat']);
+            tmp = UpMat (RHO);
+            for num = 1: length(tmp)
+                X { k,  num } = tmp {num};
+            end
+            Y  {k, 1}  = 'ASD';
+            k=k+1;
+        elseif strcmp(Group(g),'NC')
+            load([ 'CSpecE_data\NC_CSpecE_', num2str(nSub), '.mat']);
+            tmp = UpMat (RHO);
+            for num = 1: length(tmp)
+                X { k,  num } = tmp {num};
+            end
+            Y  {k, 1}  = 'NC';
+            k=k+1;
+        end
+
+    end
+
+end
+
+save('ASD_NC_CSpecE.mat','X','Y');
+close(f);
